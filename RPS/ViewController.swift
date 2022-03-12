7ae9ce5c8ae0c86ba96e59fc8305e6808c2f6f43
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-12 15:10:54 -0500

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Mackenzie Willard on 3/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
