adacb1a2dc020ba49db2ceb1a7744aaf8b27de76
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-12 15:26:07 -0500

    new enumerations for the different game states

---
+++ RPS/GameState.swift
@@ -0,0 +1,15 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Mackenzie Willard on 3/12/22.
+//
+
+import Foundation
+
+enum GameState{
+    case start
+    case win
+    case lose
+    case draw
+}
