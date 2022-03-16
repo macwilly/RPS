847015fd143ade81dbf48295c7f674c93d7a4200
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-15 20:22:02 -0400

    Actions and Outlets for different UI elements

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var appsSign: UILabel!
+    @IBOutlet weak var gameStatus: UILabel!
+    @IBOutlet weak var playerSignRock: UIButton!
+    @IBOutlet weak var playerSignPaper: UIButton!
+    @IBOutlet weak var playerSignScissor: UIButton!
+    @IBOutlet weak var playAgain: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func playerSignRockAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func playerSignPaperAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func playerSignScissorAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func playAgainAction(_ sender: Any) {
+    }
+    
 }
 
