7bca1babc1f55d6c957c052517947f35c06b5374
Mackenzie Willard <mtwillard29@gmail.com>
2022-03-18 17:47:41 -0400

    Updating enum to return the color and messages based on the state of the game

--- RPS/GameState.swift
+++ RPS/GameState.swift
@@ -6,10 +6,37 @@
 //
 
 import Foundation
+import UIKit
 
 enum GameState{
     case start
     case win
     case lose
     case draw
+    
+    var stateMessage: String {
+        switch self {
+        case .start:
+            return "Pick a sign"
+        case .win:
+            return "You won congradulations"
+        case .lose:
+            return "You lost, Play again?"
+        case .draw:
+            return "It was a draw."
+        }
+    }
+    
+    var stateColor: UIColor{
+        switch self {
+        case .start:
+            return .white
+        case .win:
+            return .green
+        case .lose:
+            return .red
+        case .draw:
+            return .yellow
+        }
+    }
 }
